6889fe04e2a6a6dd2399f75caa440daf3c895c40
Erin Lim <erinlim@utexas.edu>
2021-06-24 22:24:25 -0500

    edits on js

--- Develop/script.js
+++ Develop/script.js
@@ -4,17 +4,13 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
-var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "~", "-", "_", "+","=", "<", ">", "?", "/", "`", "."];
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-var userLength;
-var confirmNumber;
-var confirmSpecial;
-var confirmLower;
-var cofirmUpper;
+
 var passwordchar = [];
-var randomPassword = "";
+
 
 function generatePassword() {
   var userLength = prompt("How many character would you like your password? (min 8, max 128)");
@@ -38,10 +34,10 @@ function generatePassword() {
     passwordchar = alert("You must include at least one of the options.");
   }
   if (confirmLower) {
-    passwordchar = passwordchar.concat(alphaLower);
+    passwordchar = passwordchar.concat(lower);
   }
   if (confirmUpper) {
-    passwordchar = passwordchar.concat(alphaUpper);
+    passwordchar = passwordchar.concat(upper);
   }
   if (confirmNumber) {
     passwordchar = passwordchar.concat(number);
@@ -49,18 +45,15 @@ function generatePassword() {
   if (confirmSpecial) {
     passwordchar = passwordchar.concat(specialChar);
   }
-  console.log(passwordchar);
 
-  
+  var randomPassword = "";
 
   for (var i = 0; i < userLength; i++) {
     randomPassword = randomPassword + passwordchar[Math.floor(Math.random() * passwordchar.length)];
-    console.log(randomPassword);
   }
   return randomPassword;
-
-
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
